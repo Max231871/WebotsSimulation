5398815234ecaf126da562db48df373c02d89550
ayaan2340 <85899570+ayaan2340@users.noreply.github.com>
2024-11-18 18:49:03 -0600

    Add fitness score calculation

--- controllers/AyanManualDrive/model.py
+++ controllers/AyanManualDrive/model.py
@@ -3,3 +3,8 @@ import random
 def getInstructions(speed, steering, position, image, endPos, time):
     print(time)
     return random.randint(1, 30), (random.random() - 0.5);
+    
+    
+def computeFitnessScore(shortestDistance, time, maxSpeed):
+     minTime = shortestDistance / maxSpeed
+     return minTime / time;
